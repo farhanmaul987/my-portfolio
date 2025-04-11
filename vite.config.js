6a4e5832508e5532f9c466d69bdded4fd0d0f7dd
Farhan Maulana <farhanmaul987@gmail.com>
2025-04-11 22:53:05 +0700

    Update Vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
@@ -7,6 +12,10 @@ export default defineConfig({
   build: {
     outDir: "dist",
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        fallback: resolve(__dirname, "index.html"), // generate 404.html dari index.html
+      },
       output: {
         entryFileNames: "assets/[name]-[hash].js",
         chunkFileNames: "assets/[name]-[hash].js",
