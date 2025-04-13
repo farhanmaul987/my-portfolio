69fe196156f6a5df6674150d4621d6f8b5f19d45
Farhan Maulana <farhanmaul987@gmail.com>
2025-04-13 15:29:29 +0700

    Update for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,7 @@
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Pastikan base path benar sesuai dengan domain
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        fallback: resolve(__dirname, "index.html"), // generate 404.html dari index.html
-      },
-      output: {
-        entryFileNames: "assets/[name]-[hash].js",
-        chunkFileNames: "assets/[name]-[hash].js",
-        assetFileNames: "assets/[name]-[hash].[ext]",
-      },
-    },
-  },
+  base: "/",
 });
